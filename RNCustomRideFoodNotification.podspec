96f42536d89144b9296b05b1c8f00308be7274f7
Andre Piropo Santos <piroposantosdev@gmail.com>
2025-12-16 22:04:15 -0300

    chore: Relocate iOS LiveActivityModule files and update project configurations for packaging.

--- RNCustomRideFoodNotification.podspec
+++ RNCustomRideFoodNotification.podspec
@@ -14,6 +14,7 @@ Pod::Spec.new do |s|
   s.source       = { :git => "https://github.com/piroposantosdev/RNCustomRideFoodNotification.git", :tag => "#{s.version}" }
 
   s.source_files = "ios/**/*.{h,m,mm,swift}"
+  s.exclude_files = ["ios/RideFoodWidgetBundle.swift", "ios/RideFoodActivityWidget.swift"]
 
   s.dependency "React-Core"
 end
--- example/ios/RNCustomRideFoodNotification/AppDelegate.swift
+++ example/ios/RNCustomRideFoodNotification/AppDelegate.swift
@@ -40,7 +40,7 @@ class ReactNativeDelegate: RCTDefaultReactNativeFactoryDelegate {
 
   override func bundleURL() -> URL? {
 #if DEBUG
-    RCTBundleURLProvider.sharedSettings().jsBundleURL(forBundleRoot: "index")
+    return URL(string: "http://192.168.1.51:8081/index.bundle?platform=ios&dev=true")
 #else
     Bundle.main.url(forResource: "main", withExtension: "jsbundle")
 #endif
